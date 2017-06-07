65e7cddb3b44249c37fd9ba28a2ce8731f0e195c
Chris Umbel <chris@chrisumbel.com>
2017-06-07 07:25:08 -0400

    transaction

--- src/main/java/com/chrisumbel/integrations/jmstoamqpsample/App.java
+++ src/main/java/com/chrisumbel/integrations/jmstoamqpsample/App.java
@@ -1,10 +1,13 @@
 package com.chrisumbel.integrations.jmstoamqpsample;
 
+import javax.jms.Session;
+
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.transaction.RabbitTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,20 +20,19 @@ import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.MessageSources;
 import org.springframework.integration.dsl.amqp.Amqp;
 import org.springframework.integration.dsl.core.Pollers;
+import org.springframework.integration.dsl.jms.Jms;
+import org.springframework.jms.listener.DefaultMessageListenerContainer;
 
 @SpringBootApplication
 public class App {	
 	@Autowired
 	private javax.jms.ConnectionFactory jmsConnectionFactory;
-	
-	@Autowired
-	private RabbitTemplate amqpTemplate;	
-	
+		
 	@Bean
-	@Profile("local")
+//	@Profile("local")
 	public org.springframework.amqp.rabbit.connection.ConnectionFactory defaultConnectionFactory() {
-	    CachingConnectionFactory cf = new CachingConnectionFactory("RABBITMQ-HOST");
-	    cf.setAddresses("RABBITMQ-HOST");
+	    CachingConnectionFactory cf = new CachingConnectionFactory("localhost");
+//	    cf.setAddresses("localhost");
 	    cf.setUsername("guest");
 	    cf.setPassword("guest");
 	    cf.setVirtualHost("/");
@@ -38,24 +40,41 @@ public class App {
 	}	
 	
 	@Bean 
-	@Profile("local")
-	javax.jms.ConnectionFactory activemqConnectionFactory() 
+	public RabbitTemplate amqpTemplate(org.springframework.amqp.rabbit.connection.ConnectionFactory connectionFactory) {
+		RabbitTemplate template = new RabbitTemplate(connectionFactory);
+		template.setChannelTransacted(true);
+		return template;
+	}	
+	
+	@Bean 
+//	@Profile("local")
+	public javax.jms.ConnectionFactory activemqConnectionFactory() 
 	{
-		return new ActiveMQConnectionFactory("tcp://ACTIVEMQ-HOST:61616");
+		return new ActiveMQConnectionFactory("tcp://localhost:61616");
 	}
-		
+
+	@Bean
+	public  RabbitTransactionManager rabbitTransactionManager() { 
+		return new RabbitTransactionManager(defaultConnectionFactory());		
+	}
+
+	@Autowired
+	RabbitTransactionManager transactionManager;	
+	
 	@Bean(name = PollerMetadata.DEFAULT_POLLER)
 	public PollerMetadata poller() {
-		return Pollers.fixedRate(500).get();
+
+		return Pollers.fixedRate(500).transactional(transactionManager).get();
 	}	
 
 	@Bean
-	public IntegrationFlow qIntegrationFlow(SampleEndpoint endpoint) {		
-		return IntegrationFlows.from((MessageSources s) -> s.jms(this.jmsConnectionFactory).destination("queue1"))
-				.handle(endpoint)
-				.handle(Amqp.outboundAdapter(this.amqpTemplate).routingKey("shunted")).get();
+	public IntegrationFlow qIntegrationFlow(RabbitTemplate amqpTemplate) {		
+		DefaultMessageListenerContainer container = Jms.container(this.jmsConnectionFactory, "queue1").transactionManager(transactionManager).get();
+		
+		return IntegrationFlows.from(Jms.messageDrivenChannelAdapter(container))
+				.handle(Amqp.outboundAdapter(amqpTemplate).routingKey("shunted")).get();
 	}
-	
+		
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(App.class, args);
 	}
